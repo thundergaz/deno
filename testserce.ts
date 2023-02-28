7642e6d0ba10007d6f201e3040a165dc486ccf74
v_puzyuan <wqiosa@163.com>
2023-02-28 17:06:12 +0800

    增加路陆

--- testserce.ts
+++ testserce.ts
@@ -1,3 +1,16 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
-serve((req: Request) => new Response("Hello World1"));
+const BOOK_ROUTE = new URLPattern({ pathname: "/books/:id" });
+
+function handler(req: Request): Response {
+  const match = BOOK_ROUTE.exec(req.url);
+  if (match) {
+    const id = match.pathname.groups.id;
+    return new Response(`Book ${id}`);
+  }
+  return new Response("Not found (try /books/1)", {
+    status: 404,
+  });
+}
+
+serve(handler);
