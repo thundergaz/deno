3fca0654e99433323665a2e85134dcc8361b999d
v_puzyuan <wqiosa@163.com>
2023-03-02 09:27:47 +0800

    提取出公共方法

--- testserce.ts
+++ testserce.ts
@@ -3,6 +3,7 @@ import {
     serve,
     validateRequest,
   } from "https://deno.land/x/sift@0.6.0/mod.ts";
+import { queryFauna } from "./query";
   
   serve({
     "/quotes": handleQuotes,
@@ -109,50 +110,3 @@ import {
   
     return createQuote; // {quote: "*", author: "*"}
   }
-  
-  type FaunaError = {
-    message: string;
-  };
-  
-  /** Query FaunaDB GraphQL endpoint with the provided query and variables. */
-  async function queryFauna(
-    query: string,
-    variables: { [key: string]: unknown },
-  ): Promise<{
-    data?: unknown;
-    errors?: FaunaError[];
-  }> {
-    // Grab the secret from the environment.
-    const token = Deno.env.get("FAUNA_SECRET");
-    if (!token) {
-      throw new Error("environment variable FAUNA_SECRET not set");
-    }
-  
-    try {
-      // Make a POST request to fauna's graphql endpoint with body being
-      // the query and its variables.
-      const res = await fetch("https://graphql.fauna.com/graphql", {
-        method: "POST",
-        headers: {
-          authorization: `Bearer ${token}`,
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          query,
-          variables,
-        }),
-      });
-  
-      const { data, errors } = await res.json();
-      if (errors) {
-        // Return the first error if there are any.
-        return { data, errors };
-      }
-  
-      return { data };
-    } catch (error) {
-      console.error(error);
-      return { errors: [{ message: "failed to fetch data from fauna" }] };
-    }
-  }
-  
