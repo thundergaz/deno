80c046791708d8df283bf33a05d6c5ae9b9c4697
sturat <wqiosa@163.com>
2023-05-11 01:16:47 +0800

    增加了根据日期查询的接口

--- src/controller/rbyt.controller.ts
+++ src/controller/rbyt.controller.ts
@@ -4,29 +4,29 @@ import { client, query } from "../database/db.ts";
 const addScoreController = async ({ request, response }: RouterContext<string>) => {
   const { item, date } = await request.body().value;
   const result = await client
-  .query(
-    query.Create(query.Collection("rbyt"), {
-      data: {
-        // 加分项
-        item,
-        // 加分的时间
-        date
+    .query(
+      query.Create(query.Collection("rbyt"), {
+        data: {
+          // 加分项
+          item,
+          // 加分的时间
+          date
+        }
       }
-    }
+      )
     )
-  )
-  .then((ret) => {
-    return {
-      ...ret,
-      success: true
-    }
-  })
-  .catch((err) => {
-    return {
-      ...err,
-      success: false
-    }
-  });
+    .then((ret) => {
+      return {
+        ...ret,
+        success: true
+      }
+    })
+    .catch((err) => {
+      return {
+        ...err,
+        success: false
+      }
+    });
   if (result.success) {
     response.status = 200;
   } else {
@@ -37,35 +37,35 @@ const addScoreController = async ({ request, response }: RouterContext<string>) 
 
 const scoreListController = async ({ state, response }: RouterContext<string>) => {
   const result = await client
-  .query(
-    query.Map(
-      query.Paginate(query.Match(query.Index("rbyt_all_score"))),
-      query.Lambda('scoreRef', 
-        query.Let(
-          {
-            shipDoc: query.Get(query.Var("scoreRef"))
-          },
-          {
-            id: query.Select(["ref", "id"], query.Var("shipDoc")),
-            item: query.Select(["data", "item"], query.Var("shipDoc")),
-            date: query.Select(["data", "date"], query.Var("shipDoc")),
-          }
+    .query(
+      query.Map(
+        query.Paginate(query.Match(query.Index("rbyt_all_score"))),
+        query.Lambda('scoreRef',
+          query.Let(
+            {
+              shipDoc: query.Get(query.Var("scoreRef"))
+            },
+            {
+              id: query.Select(["ref", "id"], query.Var("shipDoc")),
+              item: query.Select(["data", "item"], query.Var("shipDoc")),
+              date: query.Select(["data", "date"], query.Var("shipDoc")),
+            }
+          )
         )
       )
     )
-  )
-  .then((ret) => {
-    return {
-      ...ret,
-      success: true
-    }
-  })
-  .catch((err) => {
-    return {
-      ...err,
-      success: false
-    }
-  });
+    .then((ret) => {
+      return {
+        ...ret,
+        success: true
+      }
+    })
+    .catch((err) => {
+      return {
+        ...err,
+        success: false
+      }
+    });
   if (result.success) {
     response.status = 200;
   } else {
@@ -74,4 +74,42 @@ const scoreListController = async ({ state, response }: RouterContext<string>) =
   response.body = result;
 };
 
-export default { addScoreController, scoreListController };
+const getContentController = async ({ state, response }: RouterContext<string>) => {
+  const searchDate = state.date;
+  const result = await client
+    .query(
+      query.Map(
+        query.Filter(
+          query.Paginate(query.Match(query.Index("rbyt_all_score"))),
+          query.Lambda(
+            "planetRef",
+            query.ContainsStr(
+              query.Select(["data", "date"], query.Get(query.Var("planetRef"))),
+              searchDate
+            )
+          )
+        ),
+        query.Lambda("planetRef", query.Get(query.Var("planetRef")))
+      )
+    )
+    .then((ret) => {
+      return {
+        ...ret,
+        success: true
+      }
+    })
+    .catch((err) => {
+      return {
+        ...err,
+        success: false
+      }
+    });
+  if (result.success) {
+    response.status = 200;
+  } else {
+    response.status = 500;
+  }
+  response.body = result;
+};
+
+export default { addScoreController, scoreListController, getContentController };
--- src/routes/rbyt.routes.ts
+++ src/routes/rbyt.routes.ts
@@ -5,5 +5,6 @@ const router = new Router();
 
 router.post<string>("/addScore", rbytController.addScoreController);
 router.get<string>("/scoreList", rbytController.scoreListController);
+router.get<string>("/content", rbytController.getContentController);
 
 export default router;
